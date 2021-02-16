a6df58865aaf0f547c6ce4f55c6d549f78731693
Dat-Hoang-Vo <78842761+Dat-Hoang-Vo@users.noreply.github.com>
2021-02-16 01:39:26 -0800

    Add files via upload

--- income.js
+++ income.js
@@ -9,20 +9,6 @@ function getMonthlyProductionIncome() {
     var autonomy = 1 - document.getElementById("provincial_autonomy_production").value;
 
     var overall_production_efficiency = 1 + document.getElementById("production_efficiency").value;
-    var provincial_production_efficiency = 1;
-
-
-    if (document.getElementById("no_production_building").checked == true) {
-        provincial_production_efficiency += 0;
-    } else if (document.getElementById("workshop").checked == true) {
-        provincial_production_efficiency += 0.5;
-    } else if (document.getElementById("counting_house").checked == true) {
-        provincial_production_efficiency += 1;
-    }
-
-    if (document.getElementById("seat").checked == true) {
-        provincial_production_efficiency += 0.1;
-    }
 
     var trade_control = document.getElementById("trade_control").value;
     var trade_efficiency = 1 + document.getElementById("trade_efficiency").value;
@@ -34,7 +20,7 @@ function getMonthlyProductionIncome() {
     var goods_produced = goods_modifier * 0.2 * goods_value;
 
 
-    var monthly_production_income = (goods_produced * overall_production_efficiency * provincial_production_efficiency * autonomy) / 12;
+    var monthly_production_income = (goods_produced * overall_production_efficiency * autonomy) / 12;
     var monthly_trade_income = (goods_produced * trade_efficiency * trade_control) / 12;
 
     document.getElementById("total_monthly_production").innerHTML = monthly_trade_income + monthly_production_income;
