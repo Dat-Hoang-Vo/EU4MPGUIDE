ab2aaa8dd93d8986bd3f8675afb7acb4f2cede6b
Dat-Hoang-Vo <78842761+Dat-Hoang-Vo@users.noreply.github.com>
2021-02-16 00:40:01 -0800

    Add files via upload

---
+++ income.js
@@ -0,0 +1,41 @@
+function getMonthlyTaxIncome() {
+    var efficiency = document.getElementById("provincial_tax_efficiency").value;
+    var autonomy = 1 - document.getElementById("provincial_autonomy").value;
+
+    document.getElementById("total_monthly_tax").innerHTML = (1 / 12) * efficiency * autonomy;
+}
+
+function getMonthlyProductionIncome() {
+    var autonomy = 1 - document.getElementById("provincial_autonomy_production").value;
+
+    var overall_production_efficiency = 1 + document.getElementById("production_efficiency").value;
+    var provincial_production_efficiency = 1;
+
+
+    if (document.getElementById("no_production_building").checked == true) {
+        provincial_production_efficiency += 0;
+    } else if (document.getElementById("workshop").checked == true) {
+        provincial_production_efficiency += 0.5;
+    } else if (document.getElementById("counting_house").checked == true) {
+        provincial_production_efficiency += 1;
+    }
+
+    if (document.getElementById("seat").checked == true) {
+        provincial_production_efficiency += 0.1;
+    }
+
+    var trade_control = document.getElementById("trade_control").value;
+    var trade_efficiency = 1 + document.getElementById("trade_efficiency").value;
+
+
+    var goods_value = document.getElementById("goods_value").value;
+    var goods_modifier = 1 + document.getElementById("goods_produced").value;
+
+    var goods_produced = goods_modifier * 0.2 * goods_value;
+
+
+    var monthly_production_income = (goods_produced * overall_production_efficiency * provincial_production_efficiency * autonomy) / 12;
+    var monthly_trade_income = (goods_produced * trade_efficiency * trade_control) / 12;
+
+    document.getElementById("total_monthly_production").innerHTML = monthly_trade_income + monthly_production_income;
+}
